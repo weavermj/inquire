8900e0f3aed586c6db5818ddbf8b2b044524693c
Matt Weaver <weavermj@Macintosh.local>
2014-09-28 20:08:29 +0100

    Adds new module for the inquire logo.

---
+++ modules/mod_inquire_logo_large/helper.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Helper class for Inquire Logo Large module
+ *
+ * @package    Inquire
+ * @subpackage Modules
+ */
+class modInquireLogoLarge
+{
+    /**
+     * Retrieves the logo url
+     *
+     * @param array $params An object containing the module parameters
+     * @access public
+     */
+    public static function getHello( $params )
+    {
+        return 'logo path here';
+    }
+}
+?>
---
+++ modules/mod_inquire_logo_large/mod_inquire_logo_large.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *
+ * Inquire Logo Large Module Entry Point
+ *
+ * @package    Inquire
+ * @subpackage Modules
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+// Include the syndicate functions only once
+require_once( dirname(__FILE__) . '/helper.php' );
+
+$logo = modInquireLogoLarge::getLogo($params);
+require( JModuleHelper::getLayoutPath('mod_inquire_logo_large'));
+?>
