a2e873a41fdee0d4319ab0e5a01da3ecb477ba0f
Matt Weaver <weavermjw@googlemail.com>
2014-09-28 19:55:54 +0000

    Adds local path to image.

--- modules/mod_inquire_logo_large/tmpl/default.php
+++ modules/mod_inquire_logo_large/tmpl/default.php
@@ -2,9 +2,8 @@
 // No direct access
 defined('_JEXEC') or die; ?>
 <?php
-echo $templatePath;
 if($isHomepage) { ?>
-    <img class="main-logo" src="logo.png">
+    <img class="main-logo" src="./images/logo.png">
     <?php
     echo $logo;
 } else {
