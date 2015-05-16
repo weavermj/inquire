18539f4ff111b222a01216836fffc2e78e31a545
Matt Weaver <matthew@weavermj.co.uk>
2015-05-16 22:11:30 +0100

    Adds better 'log in to read more' markup.

--- templates/joomberui/html/com_content/article/default.php
+++ templates/joomberui/html/com_content/article/default.php
@@ -221,23 +221,33 @@ if (!empty($this->item->pagination) AND $this->item->pagination && !$this->item-
 	<?php if ($params->get('show_readmore') && $this->item->fulltext != null) :
 		$link1 = JRoute::_('index.php?option=com_users&view=login');
 		$link = new JURI($link1);?>
-		<a class="button small radius info" href="<?php echo $link; ?>">
-			<?php $attribs = json_decode($this->item->attribs);  ?>
-			<?php
-			if ($attribs->alternative_readmore == null) :
-				echo JText::_('COM_CONTENT_REGISTER_TO_READ_MORE');
-			elseif ($readmore = $this->item->alternative_readmore) :
-				echo $readmore;
-				if ($params->get('show_readmore_title', 0) != 0) :
-					echo JHtml::_('string.truncate', ($this->item->title), $params->get('readmore_limit'));
-				endif;
-			elseif ($params->get('show_readmore_title', 0) == 0) :
-				echo JText::sprintf('COM_CONTENT_READ_MORE_TITLE');
-			else :
-				echo JText::_('COM_CONTENT_READ_MORE');
-				echo JHtml::_('string.truncate', ($this->item->title), $params->get('readmore_limit'));
-			endif; ?>
-		</a>
+		<div class="panel">
+			<div class="row">
+				<div class="small-12 medium-8 large-5 columns">
+					<p><?php echo JText::_('COM_CONTENT_REGISTER_TO_READ_MORE'); ?></p>
+				</div>
+
+				<div class="small-12 medium-4 large-7 columns">
+					<a class="button small radius info" href="<?php echo $link; ?>">
+						<?php $attribs = json_decode($this->item->attribs);  ?>
+						<?php
+						if ($attribs->alternative_readmore == null) :
+							echo JText::_('COM_CONTENT_REGISTER_TO_READ_MORE_BUTTON');
+						elseif ($readmore = $this->item->alternative_readmore) :
+							echo $readmore;
+							if ($params->get('show_readmore_title', 0) != 0) :
+								echo JHtml::_('string.truncate', ($this->item->title), $params->get('readmore_limit'));
+							endif;
+						elseif ($params->get('show_readmore_title', 0) == 0) :
+							echo JText::sprintf('COM_CONTENT_READ_MORE_TITLE');
+						else :
+							echo JText::_('COM_CONTENT_READ_MORE');
+							echo JHtml::_('string.truncate', ($this->item->title), $params->get('readmore_limit'));
+						endif; ?>
+					</a>
+				</div>
+			</div>
+		</div>
 	<?php endif; ?>
 	<?php endif; ?>
 	<?php if ($user->id && !$params->get('access-view')):?>
