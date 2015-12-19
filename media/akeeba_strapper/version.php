680028f99f1d9fd47aaba61ecaf3a6d48d6a359b
Matt Weaver <matthew@weavermj.co.uk>
2015-12-19 19:33:23 +0000

    Adds Akeba backup source

---
+++ media/akeeba_strapper/version.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *  @package     AkeebaStrapper
+ *  @copyright   Copyright (c) 2010 - 2015 Nicholas K. Dionysopoulos / Akeeba Ltd. All rights reserved.
+ *  @license     GNU General Public License version 2, or later
+ */
+
+// Protect from unauthorized access
+defined('_JEXEC') or die;
+
+define('AKEEBASTRAPPER_VERSION', '2.5.0');
+define('AKEEBASTRAPPER_DATE', '2015-12-14 15:04:47');
+define('AKEEBASTRAPPER_MEDIATAG', md5(AKEEBASTRAPPER_VERSION . AKEEBASTRAPPER_DATE));
